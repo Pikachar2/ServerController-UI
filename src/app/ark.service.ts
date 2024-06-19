daca7652859b94ddc8957bbf9486f73332f5593e
Pikachar2 <highiq2@hotmail.com>
2024-06-18 21:18:35 -0600

    unknown

--- src/app/ark.service.ts
+++ src/app/ark.service.ts
@@ -11,7 +11,7 @@ import { ArkSession } from './ArkSession';
   providedIn: 'root'
 })
 export class ArkService {
-  private serverControllerUrl = 'http://24.9.27.79:8081/ark';  // URL to web api
+  private serverControllerUrl = 'http://75.70.106.136:8081/ark';  // URL to web api
 //   private serverControllerUrl = 'http://73.78.14.133:8081/ark';  // URL to web api
   // private serverControllerUrl = 'http://192.168.1.25:8081/ark';  // URL to web api
   // private serverControllerUrl = 'http://127.0.0.1:8081/ark';  // URL to web api
